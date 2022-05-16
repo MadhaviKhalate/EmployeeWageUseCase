2a4b046b3f9f9f1f3030278d0ed1dc4ce0569219
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-16 21:30:58 +0530

    [Madhavi] committed

---
+++ EmployeeWage/EmployeeWage/EmployeeClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmployeeClass
+    {
+        Random rand = new Random();
+        public bool checkStatus()
+        {
+            int isFullTime = 1;
+            int randomValue = rand.Next(0, 2);
+            if (randomValue == isFullTime)
+            {
+                Console.WriteLine("Employee is present");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                return false;
+            }
+
+        }
+        public void dailyWage()
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HRS = 20;
+            int empHrs;
+            int empWage;
+
+            int empCheck = Convert.ToInt32(checkStatus());
+            if (empCheck == IS_FULL_TIME)
+                empHrs = 8;
+            else
+                empHrs = 0;
+            empWage = empHrs * EMP_RATE_PER_HRS;
+            Console.WriteLine("Daily employee wage " + empWage);
+
+        }
+    }
+}
---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace EmployeeWage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeClass employee = new EmployeeClass();
+            employee.checkStatus();
+            employee.dailyWage();
+        }
+    }
+}
