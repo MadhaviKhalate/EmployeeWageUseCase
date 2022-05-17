f8889b9d0419043ddf559de75576244b245c72f0
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-17 11:32:14 +0530

    [Madhavi] committed

--- EmployeeWage/EmployeeWage/EmployeeClass.cs
+++ EmployeeWage/EmployeeWage/EmployeeClass.cs
@@ -8,12 +8,18 @@ namespace EmployeeWage
 {
     internal class EmployeeClass
     {
-        Random rand = new Random();
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+        int EMP_RATE_PER_HRS = 20;
+        int empHrs;
+        int empWage;
+       
         public bool checkStatus()
         {
-            int isFullTime = 1;
-            int randomValue = rand.Next(0, 2);
-            if (randomValue == isFullTime)
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 3);
+
+            if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is present");
                 return true;
@@ -27,16 +33,18 @@ namespace EmployeeWage
         }
         public void dailyWage()
         {
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HRS = 20;
-            int empHrs;
-            int empWage;
-
-            int empCheck = Convert.ToInt32(checkStatus());
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
                 empHrs = 8;
+            }
             else
+            {
+                Console.WriteLine("Employee is absent");
                 empHrs = 0;
+            }
             empWage = empHrs * EMP_RATE_PER_HRS;
             Console.WriteLine("Daily employee wage " + empWage);
 
@@ -45,21 +53,48 @@ namespace EmployeeWage
         public void addPartTimeWage()
         {
 
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HRS = 20;
-            int empHrs;
-            int empWage;
-
+            Random rand = new Random();
             int empCheck = rand.Next(0, 3);
+
             if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present full time");
                 empHrs = 8;
+            }
             else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is present part time");
                 empHrs = 4;
+            }
             else
                 empHrs = 0;
             empWage = empHrs * EMP_RATE_PER_HRS;
             Console.WriteLine("Daily employee wage " + empWage);
         }
+
+        public void EmpWageUsingSwitch()
+        {
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 3);
+
+            switch (empCheck)
+            {
+                case 1:
+                    Console.WriteLine("Employee is present full time");
+                    empHrs = 8;
+                    break;
+
+                case 2:
+                    Console.WriteLine("Employee is present part time");
+                    empHrs = 4;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs* EMP_RATE_PER_HRS;
+            Console.WriteLine("Daily employee wage " + empWage);
+         }
     }
 }
--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -11,6 +11,7 @@ namespace EmployeeWage
             employee.checkStatus();
             employee.dailyWage();
             employee.addPartTimeWage();
+            employee.EmpWageUsingSwitch();
         }
     }
 }
